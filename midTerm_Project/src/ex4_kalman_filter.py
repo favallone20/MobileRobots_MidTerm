8c9117033b599bf3454429430742b80003cd3d92
favallone20 <f.avallone20@studenti.unisa.it>
2022-04-30 15:44:06 +0200

    [DOC] Defines comments for the exercise 4  -[ADD] Adds file config.py

--- midTerm_Project/src/ex4_kalman_filter.py
+++ midTerm_Project/src/ex4_kalman_filter.py
@@ -1,37 +1,25 @@
 #! /usr/bin/python3
-from re import X #To comment with the guys
 import numpy as np
 from math import acos, asin, pi
-from nav_msgs.msg import Odometry
 import tf
-from tf import TransformListener
 import rospy
 from sensor_msgs.msg import LaserScan, Imu
 from geometry_msgs.msg import Twist
 from rosgraph_msgs.msg import Clock
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from config import * 
 
-np.random.seed(1)
+np.random.seed(1) #Set a fixed seed for all the exercises
 rospy.init_node("node_ex4_kalman_filter")
-COORDINATES = [(0,0), (0.5,0), (0.5,-0.5), (0,0)]  #landmarks coordinates
-ANGULAR_THRESHOLD = 0.001
-POSITION_THRESHOLD = 0.01
-STANDARD_DEVIATION = 0.0001
-STANDARD_DEVIATION_MEASURE = 0.01 #To comment with the guys 
-WORLD_X_DIM = 1.91
-WORLD_Y_DIM = 1.91
-LINEAR_SPEED = 0.1
-ANGULAR_SPEED = 0.2
-current_angle = 0
-rate = rospy.Rate(100) #0.1s
+rate = rospy.Rate(100) #0.01s
 pub = rospy.Publisher("/cmd_vel",Twist,queue_size=1)
-tf_listener = TransformListener()
+
 
 def compute_angle(p1,p2):
+    ''''Computes angle between two vectors (angle in range [0, 2*pi])'''
     
     v_dist = np.subtract(p2,p1)
     v_norm = np.linalg.norm(v_dist)
- 
     angle_1 = acos(v_dist[0]/v_norm)
     angle_2 = asin(v_dist[1]/v_norm)
     
@@ -50,23 +38,6 @@ def compute_angle(p1,p2):
     
     return angle_to_follow
 
-# def get_odom_position():
-#     odom = rospy.wait_for_message("/odom",Odometry)
-#     # In this code we assume to have the right angle of the robot
-#     x =  odom.pose.pose.position.x
-#     y = odom.pose.pose.position.y
-#     return round(x,2), round(y,2)
-
-# def get_odom_yaw():
-#     odom = rospy.wait_for_message("/odom",Odometry)
-#     quaternion = (
-#     odom.pose.pose.orientation.x,
-#     odom.pose.pose.orientation.y,
-#     odom.pose.pose.orientation.z,
-#     odom.pose.pose.orientation.w)
-#     euler = tf.transformations.euler_from_quaternion(quaternion)
-#     return round(euler[2],2)
-
 def get_imu_yaw():
     imu = rospy.wait_for_message("/imu", Imu)
     quaternion = (
@@ -83,42 +54,57 @@ def laser_measure():
     angle = (angle + 2*pi) if angle < 0 else angle
     angle = int(angle*180/pi)
     scan = rospy.wait_for_message("/scan",LaserScan)
-    x = scan.ranges[360-angle-1] # + np.random.normal(0, STANDARD_DEVIATION_MEASURE)
-    y = scan.ranges[(630-angle-1) % 360] # + np.random.normal(0, STANDARD_DEVIATION_MEASURE) # 360 + 270, mi porto in zero e poi shift di 270 gradi
+    x = scan.ranges[360-angle-1] + np.random.normal(0, STANDARD_DEVIATION_MEASURE)
+    y = scan.ranges[(630-angle-1) % 360] + np.random.normal(0, STANDARD_DEVIATION_MEASURE)
     return x, y
 
 def estimate_position():
+    '''Estimates position based on the fixed world'''
+    
     x, y = laser_measure()
     x = WORLD_X_DIM - x
     y = (WORLD_Y_DIM - y) * -1
     return x, y
 
 def get_time():
+    '''Gets gazebo simulation time'''
+    
     clock = rospy.wait_for_message("/clock", Clock)
     time = clock.clock.secs + clock.clock.nsecs * 10**-9
     return time
 
 def angular_movement(current_point, target_point):
-    global current_angle
+    '''Rotates the robot toward the target point in a way that only
+    linear movement along x axis of the robot reference frame is needed'''
+    
+    #Compute the current angle
+    current_angle = get_imu_yaw()
+    current_angle = current_angle if current_angle > 0 else 2*pi + current_angle
+    
+    #Compute destination angle
     angle_to_go = round(compute_angle(current_point,target_point),2)
     angle_to_go = angle_to_go if angle_to_go > current_angle else angle_to_go + 2*pi
+    
+    #Compute total movement angle 
     angle = abs(current_angle - angle_to_go)
-    print("Current:" , current_point, "Target:", target_point)
-    print("Angle to go:", angle_to_go, "Angle:", angle)
+    
     movement_time = angle/ANGULAR_SPEED
     twist = Twist()
     twist.angular.z = ANGULAR_SPEED
     start_time = get_time()
     pub.publish(twist)
+    
     while (get_time() - start_time < movement_time):
         pub.publish(twist)
         rate.sleep()
+    
     twist.angular.z = 0
     pub.publish(twist)
-    current_angle = angle_to_go % (2*pi)
 
     
 def mover(current_point, target_point):
+    '''Moves robot from current point to target point'''
+    
     current_point = np.array(current_point).flatten()
     target_point = np.array(target_point)
     angular_movement(current_point, target_point)
@@ -135,12 +121,16 @@ def mover(current_point, target_point):
     twist.linear.x=0
     pub.publish(twist)
 
+#Kalman Filter implementation 
+
 def matrix_initialization():
+    '''Initializes Kalmann matricies'''
+    
     A = np.array([[1, 0], [0, 1]])
     B = np.zeros((2,2))
     H = np.array([[1, 0], [0, 1]])
     P = np.zeros((2,2))
-    Q = np.random.normal(0, STANDARD_DEVIATION, size=(2,2))
+    Q = np.random.normal(0, STANDARD_DEVIATION_ODOM, size=(2,2))
     R = np.random.normal(0, STANDARD_DEVIATION_MEASURE, size=(2,2))
     return A, B, H, P, Q, R 
     
@@ -151,7 +141,6 @@ def kf_predict(A, B, P, xk_1, vk_1, Qk_1):
     return xk_.T, Pk_
 
 def kf_update(xk_, H, Pk_, Rk):
-    # This method can rease an exception if Sk is not invertible
     
     IM = np.dot(H, xk_.T)
     IS = Rk + np.dot(H, np.dot(Pk_, H.T))
@@ -173,7 +162,7 @@ if __name__=="__main__":
     last_position = np.array([[0, 0]])
     xk_ = last_position 
     for i in range(len(COORDINATES)-1):
-        target_position = [np.random.normal(COORDINATES[i+1][0],0.05),np.random.normal(COORDINATES[i+1][1],0.05)]
+        target_position = [np.random.normal(COORDINATES[i+1][0],STANDARD_DEVIATION),np.random.normal(COORDINATES[i+1][1],STANDARD_DEVIATION)]
         mover(last_position, target_position)
         delta_t = np.linalg.norm(np.subtract(COORDINATES[i+1], xk_.flatten()))/LINEAR_SPEED
         B = np.array([[delta_t, 0], [0, delta_t]])
@@ -181,7 +170,6 @@ if __name__=="__main__":
         # get initial position
         last_position, Pk = kf_update(xk_, H, Pk_, Rk)
         xk_ = last_position
-        print("Last position: ",xk_)
         x.append(last_position[0][0])
         y.append(last_position[0][1])
         if i!= len(COORDINATES)-2:
