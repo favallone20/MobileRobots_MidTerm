c595d43d5487f5d0d4ce2b0e4e2aadfa0745db06
favallone20 <f.avallone20@studenti.unisa.it>
2022-04-30 11:42:04 +0200

    [REF] Refactors variables for speed  -[FIX] Fixes bug on linear trajectory but not on diagonal one

--- midTerm_Project/src/ex4_kalman_filter.py
+++ midTerm_Project/src/ex4_kalman_filter.py
@@ -13,13 +13,15 @@ import matplotlib.pyplot as plt
 
 np.random.seed(1)
 rospy.init_node("node_ex4_kalman_filter")
-COORDINATES = [(0,0), (0.7,0), (1,0.0), (1.5,0)]  #landmarks coordinates
+COORDINATES = [(0,0), (0.5,0), (0.5,0.5), (0,0)]  #landmarks coordinates
 ANGULAR_THRESHOLD = 0.001
 POSITION_THRESHOLD = 0.01
-STANDARD_DEVIATION = 0.1
-STANDARD_DEVIATION_MEASURE = 0.2 #To comment with the guys 
+STANDARD_DEVIATION = 0.0001
+STANDARD_DEVIATION_MEASURE = 0.01 #To comment with the guys 
 WORLD_X_DIM = 1.91
 WORLD_Y_DIM = 1.91
+LINEAR_SPEED = 0.1
+ANGULAR_SPEED = 0.2
 current_angle = 0
 rate = rospy.Rate(100) #0.1s
 pub = rospy.Publisher("/cmd_vel",Twist,queue_size=1)
@@ -99,9 +101,9 @@ def get_time():
 def angular_movement(current_point, target_point):
     global current_angle
     angle = abs(current_angle - round(compute_angle(current_point,target_point),2))
-    movement_time = angle/0.2
+    movement_time = angle/ANGULAR_SPEED
     twist = Twist()
-    twist.angular.z = 0.2
+    twist.angular.z = ANGULAR_SPEED
     start_time = get_time()
     pub.publish(twist)
     while (get_time() - start_time < movement_time):
@@ -118,9 +120,9 @@ def mover(current_point, target_point):
     angular_movement(current_point, target_point)
     distance_vector = np.subtract(target_point, current_point)
     distance = np.linalg.norm(distance_vector)
-    movement_time = distance/0.1  #To define this velocity that can be also different based on if I have reached the last position or not.
+    movement_time = distance/LINEAR_SPEED 
     twist = Twist()
-    twist.linear.x = 0.1
+    twist.linear.x = LINEAR_SPEED
     start_time = get_time()
     pub.publish(twist)
     while (get_time() - start_time < movement_time):
@@ -128,7 +130,6 @@ def mover(current_point, target_point):
         rate.sleep()    
     twist.linear.x=0
     pub.publish(twist)
-    return movement_time  # We need this information to define the B matrix 
 
 def matrix_initialization():
     A = np.array([[1, 0], [0, 1]])
@@ -140,22 +141,20 @@ def matrix_initialization():
     return A, B, H, P, Q, R 
     
 def kf_predict(A, B, P, xk_1, vk_1, Qk_1):
-    theta = get_imu_yaw()
-    xk_ = np.dot(A, xk_1.T) + np.dot(B, np.array([(vk_1, 0)]).T)
+    theta = 2*pi + get_imu_yaw() if get_imu_yaw() <0 else get_imu_yaw()
+    xk_ = np.dot(A, xk_1.T) + np.dot(B, np.array([(vk_1*np.cos(theta), vk_1*np.sin(theta))]).T)
     Pk_ = np.dot(A, np.dot(P, A.T)) + Qk_1
     return xk_.T, Pk_
 
 def kf_update(xk_, H, Pk_, Rk):
-    # Q deve essere piccolo perché integrato volta per volta, 
-    # R maggiore di Q_iniziale ma rimane costante (in termini di ordine di grandezza)
-    
     # This method can rease an exception if Sk is not invertible
-    Vk = np.array([estimate_position()]).T - np.dot(H, xk_.T)
-    Sk = np.dot(H, np.dot(Pk_, H.T)) + Rk
-    Kk = np.dot(Pk_, np.dot(H.T, np.linalg.inv(Sk)))
-    xk_new = xk_.T + np.dot(Kk, Vk)
-    Pk = Pk_ - np.dot(Kk, np.dot(Sk,Kk.T))
-    return xk_new.T, Pk
+    
+    IM = np.dot(H, xk_.T)
+    IS = Rk + np.dot(H, np.dot(Pk_, H.T))
+    K = np.dot(Pk_, np.dot(H.T, np.linalg.inv(IS)))
+    xk_ = xk_.T + np.dot(K, (np.array([estimate_position()]).T -IM))
+    Pk = Pk_ - np.dot(K, np.dot(IS, K.T))
+    return (xk_.T,Pk)
     
     
 
@@ -166,20 +165,23 @@ if __name__=="__main__":
     x = []
     y = []
     
-    xk_ = np.array([(0,0)])  #WARNING TO COMMENT I should do before the predict
-    last_position = np.array([0, 0])
+
+    last_position = np.array([[0, 0]])
+    xk_ = last_position 
     for i in range(len(COORDINATES)-1):
-        target_position = [np.random.normal(COORDINATES[i+1][0],STANDARD_DEVIATION),np.random.normal(COORDINATES[i+1][1],STANDARD_DEVIATION)]
-        delta_t = mover(last_position, target_position)
+        target_position = [np.random.normal(COORDINATES[i+1][0],0.05),np.random.normal(COORDINATES[i+1][1],0.05)]
+        mover(last_position, target_position)
+        delta_t = np.linalg.norm(np.subtract(COORDINATES[i+1], xk_.flatten()))/LINEAR_SPEED
         B = np.array([[delta_t, 0], [0, delta_t]])
-        xk_, Pk_ = kf_predict(A, B, Pk, xk_, 0.1, Qk) #0.1?! Pk_ or Pk?
+        xk_, Pk_ = kf_predict(A, B, Pk, xk_, LINEAR_SPEED, Qk) 
         # get initial position
         last_position, Pk = kf_update(xk_, H, Pk_, Rk)
+        xk_ = last_position
+        print("Last position: ",xk_)
         x.append(last_position[0][0])
         y.append(last_position[0][1])
         if i!= len(COORDINATES)-2:
             input("Press any key...")
-        rospy.loginfo(last_position)
 
     
     c = np.array(COORDINATES)
