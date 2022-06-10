dd58b15055090d4c9b89a3e0a18e8fbf1e9efe06
favallone20 <f.avallone20@studenti.unisa.it>
2022-06-10 12:39:28 +0200

    [REF] Refactors code for read_trajectory

--- final_Project/read_trajectory/src/read_trajectory.py
+++ final_Project/read_trajectory/src/read_trajectory.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python3
 import csv
 from tokenize import Double, PseudoExtras
-
+import tf
 from rosdep2 import RosdepLookup
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped,Quaternion
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from actionlib_msgs.msg import GoalStatusArray, GoalStatus
 import time
@@ -12,7 +12,41 @@ import time
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
+from numpy import subtract
+from numpy.linalg import norm
+from math import atan2,acos,asin,pi
 n_reached = 0
+
+def compute_angle(start_point,end_point):
+    v_diff = subtract(end_point,start_point)
+    # v_norm = norm(v_diff)
+    v_diff[0] += 0.00001
+    orientation = atan2(v_diff[1],v_diff[0])
+    return orientation
+
+# def compute_angle(p1,p2):
+#     ''''Computes angle between two vectors (angle in range [0, 2*pi])'''
+    
+#     v_dist = subtract(p2,p1)
+#     v_norm = norm(v_dist)
+#     angle_1 = acos(v_dist[0]/v_norm)
+#     angle_2 = asin(v_dist[1]/v_norm)
+    
+#     if (angle_1 >= 0 and angle_1 <= pi/2) and \
+#         (angle_2 >= 0 and angle_2 <= pi/2):
+#         angle_to_follow = angle_1
+#     elif (angle_1 > pi/2 and angle_1<= pi) and \
+#         (angle_2 > 0 and angle_2 < pi/2):
+#         angle_to_follow = angle_1
+#     elif (angle_1 > pi/2 and angle_1 < pi) and \
+#         (angle_2 > pi/-2 and angle_2 < 0):
+#         angle_to_follow = pi + abs(angle_2)
+#     elif (angle_1 > 0 and angle_1 <= pi/2) and \
+#         (angle_2 >= pi/-2 and angle_2 < 0):
+#         angle_to_follow = 2*pi + angle_2
+    
+#     return angle_to_follow
+
 
 def callback(data: GoalStatusArray):
     global n_reached 
@@ -60,35 +94,58 @@ if __name__=="__main__":
     
     print(rows[0][0], rows[1][0])
         
+    start_x = float(rows[0][0].split()[0])
+    start_y = float(rows[0][0].split()[1])
+
     start = MoveBaseGoal()
     start.target_pose.header.frame_id="map"
     start.target_pose.header.stamp = rospy.Time.now()
-    start.target_pose.pose.position.x = float(rows[0][0].split()[0])
-    start.target_pose.pose.position.y = float(rows[0][0].split()[1])
+    start.target_pose.pose.position.x = start_x
+    start.target_pose.pose.position.y = start_y
     start.target_pose.pose.orientation.w = 1
+
+    end_x = float(rows[1][0].split()[0])
+    end_y = float(rows[1][0].split()[1])
 
     end = MoveBaseGoal()
     end.target_pose.header.frame_id="map"
     end.target_pose.header.stamp = rospy.Time.now()
-    end.target_pose.pose.position.x = float(rows[1][0].split()[0])
-    end.target_pose.pose.position.y = float(rows[1][0].split()[1])
+    end.target_pose.pose.position.x = end_x
+    end.target_pose.pose.position.y = end_y
     end.target_pose.pose.orientation.w = 1
 
     print("partenza")
     client.send_goal(start)
     wait = client.wait_for_result()
     print("raggiunto")
+    time.sleep(0.01)
 
     for i in range(2, len(rows)):
         goal = MoveBaseGoal()
+
+        goal_x = float(rows[i][0].split()[0])
+        goal_y = float(rows[i][0].split()[1])
+
+        if i < (len(rows)-1):
+            next_goal_x = float(rows[i+1][0].split()[0])
+            next_goal_y = float(rows[i+1][0].split()[1])
+            yaw = compute_angle([goal_x,goal_y],[next_goal_x,next_goal_y])
+            quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
+        else:
+            yaw = compute_angle([goal_x,goal_y],[end_x,end_y])
+            quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose.position.x = float(rows[i][0].split()[0])
-        goal.target_pose.pose.position.y = float(rows[i][0].split()[1])
-        goal.target_pose.pose.orientation.w = 1
+        goal.target_pose.pose.position.x = goal_x
+        goal.target_pose.pose.position.y = goal_y
+        goal.target_pose.pose.orientation.x = quaternion[0]
+        goal.target_pose.pose.orientation.y = quaternion[1]
+        goal.target_pose.pose.orientation.z = quaternion[2]
+        goal.target_pose.pose.orientation.w = quaternion[3]
         client.send_goal(goal)
         wait = client.wait_for_result()
-        time.sleep(0.1)
+        time.sleep(0.01)
+        
 
     client.send_goal(end)
     wait = client.wait_for_result()
